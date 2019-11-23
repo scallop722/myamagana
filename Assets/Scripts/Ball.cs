dc226b9490a1735252e2f0ca978361a5c635960f
scallop722 <mkmrll722@gmail.com>
2019-11-23 19:21:02 +0900

    add goal

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public GameObject cube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cube.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var position = transform.position;
+        if (position.y < -100) {
+            transform.position = new Vector3(2, 20, 2);
+            transform.rotation = Quaternion.identity;
+            transform.Rotate(0, 0, 0);
+            
+            var rb = GetComponent<Rigidbody>();
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
+    }
+
+    void OnTriggerEnter(Collider other) {
+        Debug.Log(other.gameObject.name);
+        Debug.Log(other.gameObject.tag);
+        if (other.gameObject.name == "Goal") {
+            
+            
+            cube.SetActive(true);
+            for (int i = 0; i < 1000; i++) {
+                Instantiate(cube);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.G)){
+            transform.Rotate(50 * Time.deltaTime,0,0);
+        }
+        if(Input.GetKey(KeyCode.T)){
+            transform.Rotate(-50 * Time.deltaTime,0,0);
+        }
+        if(Input.GetKey(KeyCode.F)){
+            transform.Rotate(0,0,-50 * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.H)){
+           transform.Rotate(0,0,50 * Time.deltaTime);
+        }
+    }
+}
